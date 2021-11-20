6afae0b9f1d632e1850cd7532e7148acac56fad7
Kashish <write2amreen@gmail.com>
2021-11-20 19:44:26 +0400

    added indent, replaced square parenthesis with circle parenthesis, added double quotes, replaced elif with else

--- broken_pirate.py
+++ broken_pirate.py
@@ -3,8 +3,8 @@
 
 
 
-greeting = input("Hello, possible pirate! What's the password?")
-if greeting in ("Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?") #added double quotes after '?'
+if greeting in ("Arrr!"): #change square parenthesis to circle parenthesis
 	print("Go away, pirate.")
-else:
-    print("Greetings, hater of pirates!")
+else: #replaced 'elif' to 'else'
+    print("Greetings, hater of pirates!") #indented the print
