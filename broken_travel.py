c34a61dce49f59eec28c92d57b12a494c9881980
Kashish <write2amreen@gmail.com>
2021-11-20 19:55:04 +0400

    added parentheisis, double quotes and ':' . replaced elif with else and '&&' with 'and'

--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,11 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year = int(input("Greetings! What is your year of origin? "))
+year = int(input("Greetings! What is your year of origin? ")) #Added parenthesis after 'int' and double quotes after 'origin?' and changed '==' to '=' 
 
-if year <= 1900:
-    print ("Woah, that's the past!")
-elif year > 1900 and year < 2020:
+if year <= 1900:  #added ':' after 1900
+    print ("Woah, that's the past!") #added double quotes
+elif year > 1900 and year < 2021: #replaced '&&' with 'and'
     print ("That's totally the present!")
-else:
+else: #replaced 'elif' with else
     print ("Far out, that's the future!!")
